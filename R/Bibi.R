d72073628f21da0961710423b9eaddb2184bcb89
Anna Marion <146854389+AnnaMarionn@users.noreply.github.com>
2024-12-18 11:53:13 +0100

    Create Bibi.R

---
+++ R/Bibi.R
@@ -0,0 +1,18 @@
+## to build a lil more complex function we try to use if statement
+
+
+Bibi <- function(x){
+if(x>0)  {
+  print("Positive number")
+  }
+  else if
+# print("Positive number")
+  } else if(x==0)
+  {
+  print("Hey, don't miss around. Zero is nor negative nor positive okkkkk?????")
+  }
+# else if(x<0)
+  {
+  print("Negative number")
+  }
+}
